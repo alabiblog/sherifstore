bfdc9913574cec99a94f5a1d287d6cdb983fad3b
alabiblog <alabiworldwideblog@gmail.com>
2025-10-06 07:07:29 +0100

    freshhh

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     swcMinify:true,
+    experimental:{
+        esmExternals:true
+    },
+};
 
 export default nextConfig;
